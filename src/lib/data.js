84fa60bd7f45d4918bd81901577400f9199254b0
nusratjahan7 <nusratjahan77222@gmail.com>
2026-05-15 10:37:21 +0600

    secure our backend

--- src/lib/data.js
+++ src/lib/data.js
@@ -1,5 +1,8 @@
 export const getDestinationById = async (id) => {
     const res = await fetch(`http://localhost:5000/destination/${id}`, {
+        headers: {
+            authorization: "Logged in"
+        },
         cache: 'no-store'
     });
     const data = await res.json();
